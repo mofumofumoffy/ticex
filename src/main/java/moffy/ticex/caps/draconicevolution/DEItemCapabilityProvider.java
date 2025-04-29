3339d49726a9c09333d1e1fac492504833ffec4f
Moffy5612 <a-eihave@outlook.jp>
2025-04-29 21:05:06 +0900

    タグ、栄養、エネルギー

--- src/main/java/moffy/ticex/caps/draconicevolution/DEItemCapabilityProvider.java
+++ src/main/java/moffy/ticex/caps/draconicevolution/DEItemCapabilityProvider.java
@@ -7,18 +7,21 @@ import com.brandon3055.draconicevolution.api.capability.DECapabilities;
 import moffy.ticex.modules.TicEXRegistry;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import slimeknights.tconstruct.library.tools.capability.ToolCapabilityProvider.IToolCapabilityProvider;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 public class DEItemCapabilityProvider  implements IToolCapabilityProvider{
 
-    private EvolvedModuleHost moduleHost;
-    private EvolvedOPStorage opStorage;
+    protected EvolvedModuleHost moduleHost;
+    protected EvolvedOPStorage opStorage;
+    protected EvolvedEnergyStorage energyStorage;
 
     public DEItemCapabilityProvider(ItemStack stack, Supplier<? extends IToolStackView> toolSupplier){
         this.moduleHost = new EvolvedModuleHost(toolSupplier.get());
         this.opStorage = new EvolvedOPStorage(this.moduleHost, toolSupplier.get());
+        this.energyStorage = new EvolvedEnergyStorage(opStorage);
     }
 
     @Override
@@ -27,6 +30,8 @@ public class DEItemCapabilityProvider  implements IToolCapabilityProvider{
             return LazyOptional.of(()->moduleHost).cast();
         }else if(capability == DECapabilities.OP_STORAGE && tool.getModifierLevel(TicEXRegistry.EVOLVED_MODIFIER.get()) > 0){
             return LazyOptional.of(()->opStorage).cast();
+        }else if(capability == ForgeCapabilities.ENERGY && tool.getModifierLevel(TicEXRegistry.EVOLVED_MODIFIER.get()) > 0){
+            return LazyOptional.of(()->energyStorage).cast();
         }
         return LazyOptional.empty();
     }
---
+++ src/main/java/moffy/ticex/mixin/mekanism/ModuleNutritionalInjectionUnitMixin.java
@@ -0,0 +1,85 @@
+package moffy.ticex.mixin.mekanism;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import mekanism.api.gear.IHUDElement;
+import mekanism.api.gear.IModule;
+import mekanism.api.gear.IModuleHelper;
+import mekanism.api.math.FloatingLong;
+import mekanism.common.config.MekanismConfig;
+import mekanism.common.content.gear.mekasuit.ModuleNutritionalInjectionUnit;
+import mekanism.common.registries.MekanismFluids;
+import mekanism.common.util.MekanismUtils;
+import mekanism.common.util.StorageUtils;
+import moffy.ticex.item.modifiable.ItemModifiableMekaSuitArmor;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
+import slimeknights.tconstruct.library.tools.item.IModifiable;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+
+@Mixin(value = ModuleNutritionalInjectionUnit.class, remap = false)
+public class ModuleNutritionalInjectionUnitMixin {
+
+    @Shadow
+    @Final
+    private static ResourceLocation icon;
+
+    @Inject(
+        at = @At("head"),
+        method = "tickServer",
+        cancellable = true
+    )
+    public void tickServer(IModule<ModuleNutritionalInjectionUnit> module, Player player, CallbackInfo cb) {
+        ItemStack container = module.getContainer();
+        if(container.getItem() instanceof IModifiable){
+            FloatingLong usage = (FloatingLong)MekanismConfig.gear.mekaSuitEnergyUsageNutritionalInjection.get();
+            if (MekanismUtils.isPlayingMode(player) && player.canEat(false)) {
+                ItemModifiableMekaSuitArmor item = (ItemModifiableMekaSuitArmor)container.getItem();
+                int needed = Math.min(20 - player.getFoodData().getFoodLevel(), item.getContainedFluid(container, MekanismFluids.NUTRITIONAL_PASTE.getFluidStack(1)).getAmount() / MekanismConfig.general.nutritionalPasteMBPerFood.get());
+                int toFeed = Math.min(module.getContainerEnergy().divideToInt(usage), needed);
+                if (toFeed > 0) {
+                    module.useEnergy(player, usage.multiply((long)toFeed));
+                    FluidUtil.getFluidHandler(container).ifPresent((handler) -> {
+                    handler.drain(MekanismFluids.NUTRITIONAL_PASTE.getFluidStack(toFeed * MekanismConfig.general.nutritionalPasteMBPerFood.get()), FluidAction.EXECUTE);
+                    });
+                    player.getFoodData().eat(needed, MekanismConfig.general.nutritionalPasteSaturation.get());
+                }
+            }
+            cb.cancel();
+        }
+    }
+
+    @Inject(
+        at = @At("head"),
+        method = "addHUDElements",
+        cancellable = true
+    )
+    public void addHUDElements(IModule<ModuleNutritionalInjectionUnit> module, Player player, Consumer<IHUDElement> hudElementAdder, CallbackInfo cb) {
+        ItemStack container = module.getContainer();
+        if(container.getItem() instanceof IModifiable){
+            Optional<IFluidHandlerItem> capability = FluidUtil.getFluidHandler(container).resolve();
+            if (capability.isPresent()) {
+                IFluidHandlerItem handler = (IFluidHandlerItem)capability.get();
+                int max = MekanismConfig.gear.mekaSuitNutritionalMaxStorage.getAsInt();
+                handler.drain(MekanismFluids.NUTRITIONAL_PASTE.getFluidStack(max), FluidAction.SIMULATE);
+            }
+
+            FluidStack stored = ((ItemModifiableMekaSuitArmor)container.getItem()).getContainedFluid(container, MekanismFluids.NUTRITIONAL_PASTE.getFluidStack(1));
+            double ratio = StorageUtils.getRatio((long)stored.getAmount(), (long)MekanismConfig.gear.mekaSuitNutritionalMaxStorage.get());
+            hudElementAdder.accept(IModuleHelper.INSTANCE.hudElementPercent(icon, ratio));
+            cb.cancel();
+        }
+   }
+}
