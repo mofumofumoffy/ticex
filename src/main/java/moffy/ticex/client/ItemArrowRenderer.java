cb4343bb73ccd42994d23977ed99c871e1d3dfc7
Moffy5612 <a-eihave@outlook.jp>
2025-05-17 12:13:21 +0900

    ItemArrow

---
+++ src/main/java/moffy/ticex/client/ItemArrowRenderer.java
@@ -0,0 +1,46 @@
+package moffy.ticex.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+
+import moffy.ticex.entity.ItemArrow;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ArrowRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
+
+public class ItemArrowRenderer extends ArrowRenderer<ItemArrow>{
+
+    private final ItemRenderer itemRenderer;
+    private final float scale;
+
+    public ItemArrowRenderer(Context pContext, float pScale) {
+        super(pContext);
+        this.itemRenderer = pContext.getItemRenderer();
+        this.scale = pScale;
+    }
+
+    @Override
+    public void render(ItemArrow pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack,
+            MultiBufferSource pBuffer, int pPackedLight) {
+        if (pEntity.tickCount >= 2 || !(this.entityRenderDispatcher.camera.getEntity().distanceToSqr(pEntity) < 12.25D)) {
+            pPoseStack.pushPose();
+            pPoseStack.scale(this.scale, this.scale, this.scale);
+            pPoseStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
+            pPoseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
+            this.itemRenderer.renderStatic(pEntity.getItem(), ItemDisplayContext.GROUND, pPackedLight, OverlayTexture.NO_OVERLAY, pPoseStack, pBuffer, pEntity.level(), pEntity.getId());
+            pPoseStack.popPose();
+            super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
+        }
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(ItemArrow pEntity) {
+        return TextureAtlas.LOCATION_BLOCKS;
+    }
+    
+}
---
+++ src/main/java/moffy/ticex/entity/ItemArrow.java
@@ -0,0 +1,15 @@
+package moffy.ticex.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import slimeknights.tconstruct.tools.item.CrystalshotItem.CrystalshotEntity;
+
+public abstract class ItemArrow extends CrystalshotEntity{
+
+    public ItemArrow(EntityType<? extends CrystalshotEntity> type, Level level) {
+        super(type, level);
+    }
+    
+    public abstract ItemStack getItem();
+}
