00ed0f2df680fa800fadf2f73f1e65e0d88e69be
Moffy5612 <a-eihave@outlook.jp>
2025-06-22 15:50:39 +0900

    オートキャスト制御

--- src/main/java/moffy/ticex/lib/utils/TicEXPsiUtils.java
+++ src/main/java/moffy/ticex/lib/utils/TicEXPsiUtils.java
@@ -2,8 +2,10 @@ package moffy.ticex.lib.utils;
 
 import java.util.function.Consumer;
 
+import moffy.ticex.modifier.ModifierPsionizingRadiation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
 import vazkii.psi.api.PsiAPI;
 import vazkii.psi.api.cad.ISocketable;
 import vazkii.psi.api.spell.SpellContext;
@@ -15,8 +17,9 @@ public class TicEXPsiUtils {
     public static void CastSpell(Player player, ItemStack toolStack, Consumer<SpellContext> consumer){
         PlayerData data = PlayerDataHandler.get(player);
 		ItemStack playerCad = PsiAPI.getPlayerCAD(player);
+        ToolStack tool = ToolStack.from(toolStack);
 
-        if (!playerCad.isEmpty()) {
+        if (!playerCad.isEmpty() && tool.getPersistentData().getBoolean(ModifierPsionizingRadiation.AUTO_CASTING_LOC)) {
             ItemStack bullet = ISocketable.socketable(toolStack).getSelectedBullet();
             final ItemStack finalTool = toolStack;
             ItemCAD.cast(player.getCommandSenderWorld(), player, data, bullet, playerCad, 5, 10, 0.05F, (spellContext)->{
