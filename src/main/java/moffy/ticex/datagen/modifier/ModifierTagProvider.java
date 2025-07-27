1c0e1332068ca314dc0df3dc2925fbc650037fd5
toapuro <164835903+toapuro@users.noreply.github.com>
2025-07-27 17:55:53 +0900

    モディファイア除去のブラックリスト追加

--- src/main/java/moffy/ticex/datagen/modifier/ModifierTagProvider.java
+++ src/main/java/moffy/ticex/datagen/modifier/ModifierTagProvider.java
@@ -1,17 +1,13 @@
 package moffy.ticex.datagen.modifier;
 
-import static slimeknights.tconstruct.common.TinkerTags.Modifiers.ABILITIES;
-import static slimeknights.tconstruct.common.TinkerTags.Modifiers.BONUS_SLOTLESS;
-import static slimeknights.tconstruct.common.TinkerTags.Modifiers.DEFENSE;
-import static slimeknights.tconstruct.common.TinkerTags.Modifiers.EXTRACT_MODIFIER_BLACKLIST;
-import static slimeknights.tconstruct.common.TinkerTags.Modifiers.EXTRACT_SLOTLESS_BLACKLIST;
-import static slimeknights.tconstruct.common.TinkerTags.Modifiers.GENERAL_UPGRADES;
-
 import moffy.ticex.TicEX;
+import moffy.ticex.lib.TicEXTags;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import slimeknights.tconstruct.library.data.tinkering.AbstractModifierTagProvider;
+
+import static slimeknights.tconstruct.common.TinkerTags.Modifiers.*;
 
 public class ModifierTagProvider extends AbstractModifierTagProvider {
 
@@ -57,5 +53,10 @@ public class ModifierTagProvider extends AbstractModifierTagProvider {
                 new ResourceLocation(TicEX.MODID, "lamellar"),
                 new ResourceLocation(TicEX.MODID, "flowerstorm")
             );
+
+        this.tag(TicEXTags.Modifiers.REMOVAL_BLACKLIST).addOptional(
+                new ResourceLocation(TicEX.MODID, "overload"),
+                new ResourceLocation(TicEX.MODID, "override")
+        );
     }
 }
