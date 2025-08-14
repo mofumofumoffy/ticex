b3e8b76aa978b01f64ab5b4831ba968d25148fa3
toapuro <164835903+toapuro@users.noreply.github.com>
2025-08-14 22:23:28 +0900

    CC:Tweaked連携のバグ修正 (#122)

--- src/main/java/moffy/ticex/modifier/propeties/IncomparableProperty.java
+++ src/main/java/moffy/ticex/modifier/propeties/IncomparableProperty.java
@@ -2,10 +2,12 @@ package moffy.ticex.modifier.propeties;
 
 import dan200.computercraft.api.lua.ILuaFunction;
 import dan200.computercraft.api.lua.MethodResult;
-import moffy.ticex.lib.ResonanceTools;
 import moffy.ticex.item.modifiable.ModifiableGauntlet;
+import moffy.ticex.lib.ResonanceTools;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.util.LogicalSidedProvider;
+import net.minecraftforge.fml.LogicalSide;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,9 +27,11 @@ public class IncomparableProperty {
     }
 
     public static ILuaFunction shootTool(Player user, ItemStack stack){
-        return (iArguments -> {
-            ResonanceTools.shootGauntletStack(user, stack, false);
+        return iArguments -> {
+            LogicalSidedProvider.WORKQUEUE.get(LogicalSide.SERVER).execute(() -> {
+                ResonanceTools.shootGauntletStack(user, stack, false);
+            });
             return MethodResult.of(true);
-        });
+        };
     }
 }
