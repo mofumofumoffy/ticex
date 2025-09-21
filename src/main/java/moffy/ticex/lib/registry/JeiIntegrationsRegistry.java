1efe6694b6e1772ee33210a46e9fa71140bfe483
toapuro <164835903+toapuro@users.noreply.github.com>
2025-09-21 13:37:48 +0900

    JEI連携のクラッシュバグの修正 (#138)

--- src/main/java/moffy/ticex/lib/registry/JeiIntegrationsRegistry.java
+++ src/main/java/moffy/ticex/lib/registry/JeiIntegrationsRegistry.java
@@ -3,23 +3,39 @@ package moffy.ticex.lib.registry;
 import moffy.ticex.jei.IJeiIntegration;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.util.Lazy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class JeiIntegrationsRegistry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JeiIntegrationsRegistry.class);
     private final Map<ResourceLocation, Lazy<? extends IJeiIntegration>> entries;
-    private final Map<ResourceLocation, IJeiIntegration> integrations;
+    private final Map<ResourceLocation, IJeiIntegration> bakedIntegrations;
 
     public JeiIntegrationsRegistry() {
         this.entries = new HashMap<>();
-        this.integrations = new HashMap<>();
+        this.bakedIntegrations = new HashMap<>();
     }
 
-    public <INTEGRATION extends IJeiIntegration> void register(ResourceLocation resourceLocation, Lazy<INTEGRATION> lazySupplier) {
-        this.entries.put(resourceLocation, lazySupplier);
+    public void register(ResourceLocation resourceLocation, Class<? extends IJeiIntegration> reference) {
+        this.entries.put(resourceLocation, Lazy.of(() -> createIntegration(reference)));
+    }
+
+    public <T extends IJeiIntegration> IJeiIntegration createIntegration(Class<T> referenceClass) {
+        try {
+            Constructor<T> constructor = referenceClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            LOGGER.error("Cannot load jei integration {}", referenceClass);
+            return null;
+        }
     }
 
     // run with jei
@@ -27,15 +43,17 @@ public class JeiIntegrationsRegistry {
     public void init() {
         entries.forEach((rl, lazySupplier) -> {
             IJeiIntegration jeiIntegration = lazySupplier.get();
-            integrations.put(rl, jeiIntegration);
+            if(jeiIntegration != null) {
+                bakedIntegrations.put(rl, jeiIntegration);
+            }
         });
     }
 
     public void each(Consumer<IJeiIntegration> consumer) {
-        this.integrations.values().forEach(consumer);
+        this.bakedIntegrations.values().forEach(consumer);
     }
 
     public <T> void each(T value, BiConsumer<IJeiIntegration, T> consumer) {
-        this.integrations.values().forEach(integration -> consumer.accept(integration, value));
+        this.bakedIntegrations.values().forEach(integration -> consumer.accept(integration, value));
     }
 }
