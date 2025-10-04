37b56ac19f701aeca5b60ba45e0449a0825de9e9
toapuro <164835903+toapuro@users.noreply.github.com>
2025-10-04 17:47:35 +0900

    アーマートリムDatagen化のリバート (#148)

--- src/main/java/moffy/ticex/event/TicEXGatherDataEvent.java
+++ src/main/java/moffy/ticex/event/TicEXGatherDataEvent.java
@@ -14,7 +14,6 @@ import moffy.ticex.datagen.general.tag.FluidTagProvider;
 import moffy.ticex.datagen.general.tag.ItemTagProvider;
 import moffy.ticex.datagen.layout.TicEXStationSlotLayoutProvider;
 import moffy.ticex.datagen.material.TicEXMaterialSpriteProvider;
-import moffy.ticex.datagen.material.trim.TicEXConditionProvider;
 import moffy.ticex.datagen.material.trim.TicEXTrimMaterialProvider;
 import moffy.ticex.datagen.modifier.ModifierProvider;
 import moffy.ticex.datagen.modifier.ModifierTagProvider;
@@ -51,7 +50,7 @@ public class TicEXGatherDataEvent {
         boolean server = event.includeServer();
         boolean client = event.includeClient();
 
-        TicEXTrimMaterialProvider.register(registrySetBuilder);
+//        TicEXTrimMaterialProvider.register(registrySetBuilder);
         TicEXDamageTypeProvider.register(registrySetBuilder);
 
         DatapackBuiltinEntriesProvider registryProvider = new DatapackBuiltinEntriesProvider(packOutput, lookupProvider, registrySetBuilder, Set.of(TicEX.MODID));
@@ -103,8 +102,5 @@ public class TicEXGatherDataEvent {
 
         //tools
         generator.addProvider(server, new ToolDefinitionProvider(packOutput));
-
-        // final modifier
-        generator.addProvider(server, new TicEXConditionProvider(packOutput));
     }
 }
