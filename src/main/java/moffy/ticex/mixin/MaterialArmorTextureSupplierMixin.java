fc7a1ccca82e0175d3eab07b7117a82297ed8d37
Moffy5612 <moffy3318rami@gmail.com>
2025-10-26 16:26:29 +0900

    マージミス修正

--- src/main/java/moffy/ticex/mixin/MaterialArmorTextureSupplierMixin.java
+++ src/main/java/moffy/ticex/mixin/MaterialArmorTextureSupplierMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.InventoryMenu;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,56 +24,69 @@ import slimeknights.tconstruct.library.client.materials.MaterialRenderInfoLoader
 import slimeknights.tconstruct.library.materials.definition.MaterialVariantId;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @Mixin(value = MaterialArmorTextureSupplier.class, remap = false)
 @Debug(export = true)
 public abstract class MaterialArmorTextureSupplierMixin {
 
-    @ModifyExpressionValue(method = "materialGetter", at = @At(value = "INVOKE", target = "Lslimeknights/mantle/data/listener/ResourceValidator;test(Lnet/minecraft/resources/ResourceLocation;)Z"))
-    private static boolean ignoreValidator(boolean original) {
-        return true;
+    @Shadow
+    private static ArmorTexture tryTexture(ResourceLocation name, int color, String material) {
+        return null;
     }
 
-    @ModifyReturnValue(method = "lambda$materialGetter$2", at = {
-            @At(value = "RETURN", ordinal = 0),
-            @At(value = "RETURN", ordinal = 1)
-    })
-    private static ArmorTexture materialGetterExtension(ArmorTexture original,
-                                                        @Local(argsOnly = true) ResourceLocation name,
-                                                        @Local MaterialVariantId materialVariantId,
-                                                        @Local(ordinal = 0) int color) {
-        ShaderProvider.Armor shaderProvider = TicEXRenders.ARMOR_SHADERS.getShaderProvider(materialVariantId);
-        if(shaderProvider == null) return original;
+    @Inject(method = "materialGetter", at = @At("HEAD"), cancellable = true)
+    private static void materialGetterExtension(ResourceLocation name, CallbackInfoReturnable<Function<String, ArmorTexture>> cir) {
+        // Ignore Validator
+        cir.setReturnValue(
+                Util.memoize(materialStr -> {
+                    if (!materialStr.isEmpty()) {
+                        MaterialVariantId material = MaterialVariantId.tryParse(materialStr);
+                        ShaderProvider.Armor shaderProvider = TicEXRenders.ARMOR_SHADERS.getShaderProvider(material);
+                        Material textureMaterial = new Material(InventoryMenu.BLOCK_ATLAS, ticex$getAtlasLocation(name));
+                        int color = -1;
+                        if (material != null) {
+                            Optional<MaterialRenderInfo> infoOptional = MaterialRenderInfoLoader.INSTANCE.getRenderInfo(material);
+                            if (infoOptional.isPresent()) {
+                                MaterialRenderInfo info = infoOptional.get();
+                                ResourceLocation untinted = info.texture();
+                                if (untinted != null) {
+                                    ArmorTexture texture = tryTexture(name, -1, '_' + untinted.getNamespace() + '_' + untinted.getPath());
+                                    if (texture != ArmorTexture.EMPTY) {
+                                        return ticex$getArmorTexture(texture, textureMaterial, color, shaderProvider, material);
+                                    }
+                                }
+                                color = info.vertexColor();
+                                for (String fallback : info.fallbacks()) {
+                                    ArmorTexture texture = tryTexture(name, color, '_' + fallback);
+                                    if (texture != ArmorTexture.EMPTY) {
+                                        return ticex$getArmorTexture(texture, textureMaterial, color, shaderProvider, material);
+                                    }
+                                }
+                            }
 
-        Material textureMaterial = new Material(InventoryMenu.BLOCK_ATLAS, ticex$getAtlasLocation(name));
-        return ticex$getArmorTexture(original, textureMaterial, color, shaderProvider);
-    }
 
-    @Inject(method = "lambda$materialGetter$2", at = @At(value = "INVOKE", target = "Ljava/util/Optional;isPresent()Z"), cancellable = true)
-    private static void materialGetterExtension$3(ResourceLocation name, String materialStr, CallbackInfoReturnable<ArmorTexture> cir,
-                                                  @Local MaterialVariantId material) {
-        Optional<MaterialRenderInfo> infoOptional = MaterialRenderInfoLoader.INSTANCE.getRenderInfo(material);
-        if(infoOptional.isEmpty()) {
-            ShaderProvider.Armor shaderProvider = TicEXRenders.ARMOR_SHADERS.getShaderProvider(material);
-            if(shaderProvider == null) return;
+                            return ticex$getArmorTexture(
+                                    new TintedArmorTexture(ArmorTextureSupplier.getTexturePath(name), -1),
+                                    textureMaterial,
+                                    -1,
+                                    shaderProvider,
+                                    material
+                            );
+                        }
 
-            Material textureMaterial = new Material(InventoryMenu.BLOCK_ATLAS, ticex$getAtlasLocation(name));
-
-            cir.setReturnValue(
-                    ticex$getArmorTexture(
-                            new TintedArmorTexture(ArmorTextureSupplier.getTexturePath(name), -1),
-                            textureMaterial,
-                            -1,
-                            shaderProvider
-                    )
-            );
-        }
+                        // base material guaranteed to exist, else we would not be in this function
+                        return new TintedArmorTexture(ArmorTextureSupplier.getTexturePath(name), color);
+                    }
+                    return ArmorTexture.EMPTY;
+                })
+        );
     }
 
     @Unique
-    private static ArmorTexture ticex$getArmorTexture(ArmorTexture texture, Material material, int color, ShaderProvider.Armor shaderProvider) {
+    private static ArmorTexture ticex$getArmorTexture(ArmorTexture texture, Material textureMaterial, int color, ShaderProvider.Armor shaderProvider, MaterialVariantId material) {
         if (shaderProvider != null && TicEXConfig.USE_SHADER.get()) {
-            return new TintedShaderArmorTexture(material, color, shaderProvider);
+            return new TintedShaderArmorTexture(textureMaterial, color, shaderProvider, material);
         }
         return texture;
     }
@@ -81,4 +95,25 @@ public abstract class MaterialArmorTextureSupplierMixin {
     private static ResourceLocation ticex$getAtlasLocation(ResourceLocation name) {
         return new ResourceLocation(name.getNamespace(), "tinker_armor/" + name.getPath());
     }
+
+    // Traveler's Updateでのみ動作
+        /*
+        @ModifyExpressionValue(method = "materialGetter", at = @At(value = "INVOKE", target = "Lslimeknights/mantle/data/listener/ResourceValidator;test(Lnet/minecraft/resources/ResourceLocation;)Z"))
+        private static boolean ignoreValidator(boolean original) {
+            return true;
+    @@ -125,9 +70,9 @@ private static ArmorTexture materialGetterExtension(ArmorTexture original,
+        }
+
+        @Unique
+        private static ArmorTexture ticex$getArmorTexture(ArmorTexture texture, Material textureMaterial, int color, ShaderProvider.Armor shaderProvider, MaterialVariantId material) {
+        private static ArmorTexture ticex$getArmorTexture(ArmorTexture texture, Material material, int color, ShaderProvider.Armor shaderProvider) {
+            if (shaderProvider != null && TicEXConfig.USE_SHADER.get()) {
+                return new TintedShaderArmorTexture(textureMaterial, color, shaderProvider, material);
+                return new TintedShaderArmorTexture(material, color, shaderProvider);
+            }
+            return texture;
+        }
+    }*/
 }
+
+
