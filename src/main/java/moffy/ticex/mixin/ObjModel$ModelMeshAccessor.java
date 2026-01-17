c17e3128607157146a031084b8f9ed0810d18cf4
Moffy5612 <moffy3318rami@gmail.com>
2026-01-17 13:55:10 +0900

    警告の除去6

--- src/main/java/moffy/ticex/mixin/ObjModel$ModelMeshAccessor.java
+++ src/main/java/moffy/ticex/mixin/ObjModel$ModelMeshAccessor.java
@@ -1,12 +1,13 @@
 package moffy.ticex.mixin;
 
 import net.minecraftforge.client.model.obj.ObjMaterialLibrary;
+import net.minecraftforge.client.model.obj.ObjModel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
 
-@Mixin(targets = "net.minecraftforge.client.model.obj.ObjModel$ModelMesh", remap = false)
+@Mixin(value = ObjModel.ModelMesh.class, remap = false)
 public interface ObjModel$ModelMeshAccessor {
     @Accessor("mat")
     ObjMaterialLibrary.Material getMaterial();
