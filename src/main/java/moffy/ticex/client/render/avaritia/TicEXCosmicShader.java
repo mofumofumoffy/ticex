6a1f310201c1925c2e736795c507bdd8aebb500c
Moffy5612 <moffy3318rami@gmail.com>
2026-02-13 14:03:17 +0900

    シェーダー最新化

--- src/main/java/moffy/ticex/client/render/avaritia/TicEXCosmicShader.java
+++ src/main/java/moffy/ticex/client/render/avaritia/TicEXCosmicShader.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.shaders.Uniform;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import committee.nova.mods.avaritia.Const;
+import committee.nova.mods.avaritia.client.AvaritiaForgeClient;
+import committee.nova.mods.avaritia.client.shader.AvaritiaShaders;
 import moffy.ticex.TicEX;
 import net.minecraft.Util;
 import net.minecraft.client.Camera;
@@ -14,6 +16,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraftforge.client.event.RegisterShadersEvent;
 
 import java.io.IOException;
@@ -37,20 +40,6 @@ public final class TicEXCosmicShader {
     public Uniform cosmicExternalScale;
     public Uniform cosmicOpacity;
     public Uniform cosmicUVs;
-
-    public final Function<ResourceLocation, float[]> cosmicUVGetter = Util.memoize(resourceLocation -> {
-        float[] cosmicUV = new float[40];
-        for (int i = 0; i < 10; ++i) {
-            TextureAtlasSprite sprite = Minecraft.getInstance()
-                    .getTextureAtlas(resourceLocation)
-                    .apply(Const.rl("misc/cosmic_" + i));
-            cosmicUV[i * 4] = sprite.getU0();
-            cosmicUV[i * 4 + 1] = sprite.getV0();
-            cosmicUV[i * 4 + 2] = sprite.getU1();
-            cosmicUV[i * 4 + 3] = sprite.getV1();
-        }
-        return cosmicUV;
-    });
 
     public TicEXCosmicShader() {
         stateShard = new RenderStateShard.ShaderStateShard(() -> shaderInstance);
@@ -125,7 +114,7 @@ public final class TicEXCosmicShader {
                 cosmicExternalScale = Objects.requireNonNull(shaderInstance.getUniform("externalScale"));
                 cosmicOpacity = Objects.requireNonNull(shaderInstance.getUniform("opacity"));
                 cosmicUVs = Objects.requireNonNull(shaderInstance.getUniform("cosmicuvs"));
-                cosmicTime.set((float) internalRenderTime + internalRenderFrame);
+                //cosmicTime.set((float) internalRenderTime + internalRenderFrame);
                 shaderInstance.apply();
                 initialize();
             }
@@ -134,7 +123,7 @@ public final class TicEXCosmicShader {
     }
 
     public void setupUniform(ResourceLocation atlas, boolean onGui) {
-        cosmicTime.set(
+        /*cosmicTime.set(
                 (System.currentTimeMillis() - internalRenderTime) / 2000.0F
         );
 
@@ -153,6 +142,26 @@ public final class TicEXCosmicShader {
         cosmicUVs.set(this.cosmicUVGetter.apply(atlas));
         cosmicYaw.set(yaw);
         cosmicPitch.set(pitch);
-        cosmicExternalScale.set(externalScale);
+        cosmicExternalScale.set(externalScale);*/
+        final Minecraft mc = Minecraft.getInstance();
+        float yaw = 0.0f;
+        float pitch = 0.0f;
+        float scale = 1f;
+        if (AvaritiaForgeClient.inventoryRender || onGui) {
+            scale = 100.0F;
+        } else {
+            yaw = (float) (mc.player.getYRot() * 2.0f * Math.PI / 360.0);
+            pitch = -(float) (mc.player.getXRot() * 2.0f * Math.PI / 360.0);
+        }
+        cosmicTime.set(mc.level.getGameTime() % Integer.MAX_VALUE);
+        cosmicYaw.set(yaw);
+        cosmicPitch.set(pitch);
+        cosmicExternalScale.set(scale);
+
+        cosmicOpacity.set(1.0F);
+
+        if (cosmicUVs != null) {
+            cosmicUVs.set(AvaritiaShaders.COSMIC_UVS);
+        }
     }
 }
