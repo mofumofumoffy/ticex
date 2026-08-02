f959147a5ce30faef5ad2432713804210583a2b6
Moffy5612 <moffy3318rami@gmail.com>
2026-08-02 21:25:24 +0900

    一部抜刀剣アドオンとの互換性を確保

--- src/main/java/moffy/ticex/mixin/slashblade/PlayerAttackHelperMixin.java
+++ src/main/java/moffy/ticex/mixin/slashblade/PlayerAttackHelperMixin.java
@@ -6,13 +6,13 @@ import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import mods.flammpfeil.slashblade.util.AttackHelper;
 import moffy.ticex.lib.hook.CriticalModifierHook;
-import moffy.ticex.mixin.CriticalAccessor;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -46,7 +46,7 @@ public abstract class PlayerAttackHelperMixin {
             @Local(argsOnly = true) LivingEntity attacker,
             @Local(argsOnly = true) Entity target
     ){
-        ItemStack stack = attacker.getItemInHand(InteractionHand.MAIN_HAND);
+        ItemStack stack = ticex_1_20_1$resolveItemInHand(attacker, InteractionHand.MAIN_HAND);
         if(stack.getItem() instanceof IModifiable && attacker instanceof Player playerAttacker) {
             CriticalModifierHook.CriticalContext criticalContext = CriticalModifierHook.modifyCritical(playerAttacker, target, isCritical, original);
             return criticalContext.criticalModifier();
@@ -59,7 +59,7 @@ public abstract class PlayerAttackHelperMixin {
                                             @Local(argsOnly = true) LivingEntity attacker,
                                             @Share(value = "context") LocalRef<ToolAttackContext> contextRef) {
         ToolAttackContext context = contextRef.get();
-        ItemStack stack = attacker.getItemInHand(context.getHand());
+        ItemStack stack = ticex_1_20_1$resolveItemInHand(attacker, context.getHand());
         if(stack.getItem() instanceof IModifiable) {
             ToolStack tool = ToolStack.from(stack);
 
@@ -79,7 +79,7 @@ public abstract class PlayerAttackHelperMixin {
                                         @Local(argsOnly = true) LivingEntity attacker,
                                         @Share(value = "context") LocalRef<ToolAttackContext> contextRef) {
         ToolAttackContext context = contextRef.get();
-        ItemStack stack = attacker.getItemInHand(context.getHand());
+        ItemStack stack = ticex_1_20_1$resolveItemInHand(attacker, context.getHand());
         if(stack.getItem() instanceof IModifiable) {
             ToolStack tool = ToolStack.from(stack);
 
@@ -97,7 +97,7 @@ public abstract class PlayerAttackHelperMixin {
                                           @Local double baseDamage,
                                           @Share(value = "context") LocalRef<ToolAttackContext> contextRef) {
         ToolAttackContext context = contextRef.get();
-        ItemStack stack = attacker.getItemInHand(context.getHand());
+        ItemStack stack = ticex_1_20_1$resolveItemInHand(attacker, context.getHand());
         if(stack.getItem() instanceof IModifiable){
             ToolStack tool = ToolStack.from(stack);
 
@@ -112,7 +112,7 @@ public abstract class PlayerAttackHelperMixin {
                                           @Local double baseDamage,
                                           @Share(value = "context") LocalRef<ToolAttackContext> contextRef) {
         ToolAttackContext context = contextRef.get();
-        ItemStack stack = attacker.getItemInHand(context.getHand());
+        ItemStack stack = ticex_1_20_1$resolveItemInHand(attacker, context.getHand());
 
         if(stack.getItem() instanceof IModifiable){
             ToolStack tool = ToolStack.from(stack);
@@ -121,5 +121,11 @@ public abstract class PlayerAttackHelperMixin {
                 entry.getHook(ModifierHooks.MELEE_HIT).failedMeleeHit(tool, entry, context, (float) baseDamage);
             }
         }
+    }
+
+    //This roundabout method is provided because some dual wielding mods apply a mixin to LivingEntity.getMainHandItem().
+    @Unique
+    private static ItemStack ticex_1_20_1$resolveItemInHand(LivingEntity attacker, InteractionHand hand){
+        return hand == InteractionHand.MAIN_HAND ? attacker.getMainHandItem() : attacker.getOffhandItem();
     }
 }
