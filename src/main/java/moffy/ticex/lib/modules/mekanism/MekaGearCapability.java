a6c4136d89fa7707160834a5d597d23935d3ec59
Moffy5612 <moffy3318rami@gmail.com>
2026-08-11 18:13:54 +0900

    未使用クラスの削除

--- src/main/java/moffy/ticex/lib/modules/mekanism/MekaGearCapability.java
+++ src/main/java/moffy/ticex/lib/modules/mekanism/MekaGearCapability.java
@@ -18,32 +18,4 @@ import java.util.List;
 public abstract class MekaGearCapability implements IMekaGear {
     public static final Capability<IMekaGear> MEKA_GEAR_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
     });
-
-    public abstract static class Provider implements ICapabilityProvider {
-
-        protected final IMekaGear mekaGearsCapability;
-        private final ItemCapabilityWrapper itemCapabilityWrapper;
-
-        public Provider(ItemStack stack){
-            this.mekaGearsCapability = getMekaGearsCapability(stack);
-            List<ItemCapabilityWrapper.ItemCapability> capabilities = new ArrayList<>();
-
-            if(mekaGearsCapability.areCapabilityConfigsLoaded()){
-                mekaGearsCapability.gatherCapabilities(stack, capabilities);
-            }
-
-            this.itemCapabilityWrapper = new ItemCapabilityWrapper(stack, capabilities.toArray(ItemCapabilityWrapper.ItemCapability[]::new));
-        }
-
-        public abstract IMekaGear getMekaGearsCapability(ItemStack stack);
-
-        @Override
-        public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction) {
-            if(capability == MEKA_GEAR_CAPABILITY){
-                return LazyOptional.of(()->this.mekaGearsCapability).cast();
-            } else {
-                return this.itemCapabilityWrapper.getCapability(capability, direction);
-            }
-        }
-    }
 }
