ebf093677425520f0e03e3f84ad4fc0748c2d4b5
Moffy5612 <moffy3318rami@gmail.com>
2026-08-14 14:15:53 +0900

    Revert "ArmorMeleeHitModifierHook追加"
    This reverts commit c52e3b99d284db7ea978c3c390fc4d8a17216564.

--- src/main/java/moffy/ticex/mixin/ToolAttackUtilMixin.java
+++ src/main/java/moffy/ticex/mixin/ToolAttackUtilMixin.java
@@ -1,75 +1,21 @@
 package moffy.ticex.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import moffy.ticex.lib.hook.CriticalModifierHook;
-import moffy.ticex.lib.utils.TicEXUtils;
-import moffy.ticex.registry.TicEXModifierHooks;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import slimeknights.tconstruct.library.modifiers.ModifierEntry;
-import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
 import slimeknights.tconstruct.library.tools.helper.ToolAttackUtil;
-import slimeknights.tconstruct.library.tools.item.IModifiable;
-import slimeknights.tconstruct.library.tools.nbt.ToolStack;
 
 import javax.annotation.Nullable;
 
 @Mixin(value = ToolAttackUtil.class, remap = false)
 public class ToolAttackUtilMixin {
-    @ModifyExpressionValue(
-            method = "performAttack",
-            at = @At(value = "INVOKE", target = "Lslimeknights/tconstruct/library/tools/context/ToolAttackContext;getBaseKnockback()F")
-    )
-    private static float performArmorMeleeHitBefore(float original, @Local(argsOnly = true)ToolAttackContext context, @Local(name = "damage") float damage){
-        float newKnockback = original;
-
-        LivingEntity attacker = context.getAttacker();
-        for(EquipmentSlot slot : TicEXUtils.EquipmentSlotLists.ARMORS){
-            ItemStack stack = attacker.getItemBySlot(slot);
-
-            if(stack.getItem() instanceof IModifiable){
-                ToolStack tool = ToolStack.from(stack);
-                for(ModifierEntry entry : tool.getModifierList()){
-                    newKnockback = entry.getHook(TicEXModifierHooks.ARMOR_MELEE_HIT).beforeMeleeHit(tool, entry, context, damage, original, newKnockback);
-                }
-            }
-        }
-
-        return newKnockback;
-    }
-
-    @ModifyExpressionValue(
-            method = "performAttack",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z")
-    )
-    private static boolean performArmorMeleeHitAfter(boolean original, @Local(argsOnly = true)ToolAttackContext context, @Local(name = "damage") float damage){
-        LivingEntity attacker = context.getAttacker();
-        for(EquipmentSlot slot : TicEXUtils.EquipmentSlotLists.ARMORS){
-            ItemStack stack = attacker.getItemBySlot(slot);
-
-            if(stack.getItem() instanceof IModifiable){
-                ToolStack tool = ToolStack.from(stack);
-                for(ModifierEntry entry : tool.getModifierList()){
-                    if(original){
-                        entry.getHook(TicEXModifierHooks.ARMOR_MELEE_HIT).afterMeleeHit(tool, entry, context, damage);
-                    } else {
-                        entry.getHook(TicEXModifierHooks.ARMOR_MELEE_HIT).failedMeleeHit(tool, entry, context, damage);
-                    }
-                }
-            }
-        }
-        return original;
-    }
-
     @WrapOperation(
             method = "getCriticalModifier",
             at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/ForgeHooks;getCriticalHit(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/entity/Entity;ZF)Lnet/minecraftforge/event/entity/player/CriticalHitEvent;")
