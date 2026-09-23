c47463d5a03431ec300723fe534b56d5fd2e6162
Moffy5612 <moffy3318rami@gmail.com>
2026-09-24 08:36:02 +0900

    バージョン変化による差異を吸収

--- src/main/java/moffy/ticex/mixin/MaterialCastingLookupMixin.java
+++ src/main/java/moffy/ticex/mixin/MaterialCastingLookupMixin.java
@@ -1,9 +1,9 @@
 package moffy.ticex.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,43 +12,47 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import slimeknights.tconstruct.library.recipe.casting.material.MaterialCastingLookup;
 import slimeknights.tconstruct.library.recipe.casting.material.MaterialFluidRecipe;
+import slimeknights.tconstruct.library.utils.SimpleCache;
 
 import java.util.*;
 
 @Mixin(value = MaterialCastingLookup.class, remap = false)
+@SuppressWarnings("deprecation")
 public class MaterialCastingLookupMixin {
 
     @Shadow
     @Final
     private static List<MaterialFluidRecipe> CASTING_FLUIDS;
 
-    @Inject(
-            at = @At("HEAD"),
-            method = "lambda$static$0",
-            cancellable = true
+    @ModifyExpressionValue(
+            method = "<clinit>",
+            at = @At(
+                    value = "NEW",
+                    target = "(Ljava/util/function/Function;)Lslimeknights/tconstruct/library/utils/SimpleCache;",
+                    ordinal = 0
+            )
     )
-    private static void modifyMaterialCastingCache(Fluid fluid, CallbackInfoReturnable<MaterialFluidRecipe> cir){
-        List<MaterialFluidRecipe> matchedRecipes = new ArrayList<>();
-        for(MaterialFluidRecipe recipe : CASTING_FLUIDS) {
-            if (recipe.matches(fluid)) {
-                matchedRecipes.add(recipe);
-            }
-        }
-
-        if(!matchedRecipes.isEmpty()){
-            for(MaterialFluidRecipe matchedRecipe: matchedRecipes){
-                ResourceLocation recipeKey = BuiltInRegistries.RECIPE_TYPE.getKey(matchedRecipe.getType());
-                ResourceLocation fluidKey = BuiltInRegistries.FLUID.getKey(fluid);
-                if(recipeKey != null && recipeKey.getNamespace().equals(fluidKey.getNamespace())){
-                    cir.setReturnValue(matchedRecipe);
-                    return;
+    private static SimpleCache<Fluid, MaterialFluidRecipe> modifyMaterialCastingCache(SimpleCache<Fluid, MaterialFluidRecipe> original){
+        return new SimpleCache<>(fluid -> {
+            List<MaterialFluidRecipe> matchedRecipes = new ArrayList<>();
+            for(MaterialFluidRecipe recipe : CASTING_FLUIDS) {
+                if (recipe.matches(fluid)) {
+                    matchedRecipes.add(recipe);
                 }
             }
 
-            cir.setReturnValue(matchedRecipes.get(0));
-            return;
-        }
+            if(!matchedRecipes.isEmpty()){
+                for(MaterialFluidRecipe matchedRecipe: matchedRecipes){
+                    ResourceLocation recipeKey = BuiltInRegistries.RECIPE_TYPE.getKey(matchedRecipe.getType());
+                    ResourceLocation fluidKey = BuiltInRegistries.FLUID.getKey(fluid);
+                    if(recipeKey != null && recipeKey.getNamespace().equals(fluidKey.getNamespace())){
+                        return matchedRecipe;
+                    }
+                }
 
-        cir.setReturnValue(MaterialFluidRecipe.EMPTY);
+                return matchedRecipes.get(0);
+            }
+            return MaterialFluidRecipe.EMPTY;
+        });
     }
 }
